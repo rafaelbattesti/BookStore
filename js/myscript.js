cb5d6509da001f505e419387cb7e1dfd0dd7f977
rafaelbattesti <rafaelbattesti@jupiter.local>
2016-06-16 00:32:08 -0400

    Remove battesti.css, left only the production theme.

---
+++ js/myscript.js
@@ -0,0 +1,67 @@
+/**
+ * Application : BookStore
+ * Name        : myscript.js
+ * Author      : rafaelbattesti
+ * Since       : 2016-06-15
+ * Version     : 0.1
+ */
+
+//Determines which information to display
+var menuId;
+
+//Keeps the data for easy access across pages (avoids multiple AJAX calls)
+var data = "Gets this value first";
+
+/*************************************************************************************************************
+ * FUNCTION DECLARATIONS                                                                                     *
+ ************************************************************************************************************/
+
+//Display an alert with error
+function showErr(e) {
+    var str = "Status: " + e.status + "\n" + "Status Text: " + e.statusText + "\n" + "Message: " + e.message;
+    alert(str);
+}
+
+//Sets home title
+function setTitle(xml) {
+    var title = $(xml).find("title").text();
+    $("title").html(title);
+}
+
+//Builds the home page header
+function buildHeader(xml) {
+    var header = $(xml).find("student").text();
+    $("header>h1").html(header);
+}
+
+//Builds the home page footer
+function buildFooter(xml) {
+    var stdNum = $(xml).find("student").attr("studentNumber");
+    var stdProg = $(xml).find("student").attr("studentProgram");
+    $("footer>h2").html(stdNum);
+    $("footer>h4").html(stdProg);
+}
+
+//Build the home page
+function buildHome(xml) {
+    data = xml;
+    setTitle(xml);
+    buildHeader(xml);
+    buildFooter(xml);
+}
+
+
+/*************************************************************************************************************
+ * EVENTS                                                                                                    *
+ ************************************************************************************************************/
+
+//pagecreate event to trigger AJAX update on home page load
+$(document).on("pagecreate", "#home", function(){
+    $.ajax({
+        type     : "GET",
+        url      : "book_site.xml",
+        dataType : "xml",
+        success  : buildHome,
+        error    : showErr
+    });
+});
